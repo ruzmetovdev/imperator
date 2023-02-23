f92d9b7b0696673430a9ee4cfee52d21fd000f1c
Sukhrobdew <sukhrob2707@gmail.com>
2023-02-23 21:55:00 +0500

    ssss

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.addEventListener("scroll", function () {
+    shrink();
+  });
+  
+  let navbar = document.getElementById("navbar");
+  
+  function shrink() {
+    if (scrollY > 100) {
+      navbar.classList.add("navbar-shrink");
+    } else {
+          navbar.classList.remove("navbar-shrink");
+      }
+  }
+
+
+//   togle--------
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
